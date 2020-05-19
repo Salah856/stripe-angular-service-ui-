fc1b6083cbd5ea59d55c1889eb980a9047c7c7d1
salah elhossiny <salah_elhossiny856@yahoo.com>
2020-05-19 22:59:33 +0200

    stripe angular form basic ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef
+} from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'stripe-angular';
+import { NgForm } from '@angular/forms';
+@Component({ ... })
+export class AppComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('cardInfo') cardInfo: ElementRef;
+  card: any;
+  cardHandler = this.onChange.bind(this);
+  error: string;
+  constructor(private cd: ChangeDetectorRef) {}
+  nngAfterViewInit() {
+    const style = {
+      base: {
+        lineHeight: '24px',
+        fontFamily: 'monospace',
+        fontSmoothing: 'antialiased',
+        fontSize: '19px',
+        '::placeholder': {
+          color: 'purple'
+        }
+      }
+    };
+  
+    this.card = elements.create('card', { style });
+    this.card.mount(this.cardInfo.nativeElement);
+  
+    this.card.addEventListener('change', this.cardHandler);
+  }
+  ngOnDestroy() {
+    this.card.removeEventListener('change', this.cardHandler);
+    this.card.destroy();
+  }
+  onChange({ error }) {
+    if (error) {
+      this.error = error.message;
+    } else {
+      this.error = null;
+    }
+    this.cd.detectChanges();
+  }
+  async onSubmit(form: NgForm) {
+    const { token, error } = await stripe.createToken(this.card, {
+      email: this.emailAddress
+    }
+
+if (error) {
+  console.log('Something is wrong:', error);
+} else {
+  console.log('Success!', token);
+  <span class="code-annotation">// ...send the token to the your backend to process the charge</span>
 }
